4a55da7bec33ed3e4e3169d3645380758e557a96
Daniel Igwilo <digwilo62@gmail.com>
2026-05-08 06:33:01 +0100

    Imroved navbar responsiveness and active navigation

--- js/web.js
+++ js/web.js
@@ -1,8 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     fetch("components/navbar.html")
+
     .then(response => response.text ())
+
     .then(data => {
+
         document.getElementById("navbar").innerHTML = data;
+
+        const currentPage = window.location.pathname.split("/").pop();
+
+        const navLinks = document.querySelectorAll(".nav-links a");
+
+        navLinks.forEach((link) => {
+
+            const linkPage = link.getAttribute("href");
+
+            if (linkPage === currentPage) {
+                link.classList.add("active");
+            }
+        });
+
     })
     .catch(error => console.error("Error loading navbar", error));
 });
@@ -23,3 +41,22 @@ const observer = new IntersectionObserver((entries) => {
 fadeElements.forEach((element) => {
     observer.observe(element);
 });
+
+window.addEventListener("scroll", function () {
+    
+    const navbar = document.querySelector(".nav-container");
+
+    if (!navbar) return;
+
+    if (window.scrollY > 40) {
+
+        navbar.style.padding = "12px 24px";
+        navbar.style.borderRadius = "14px";
+
+    } else {
+
+        navbar.style.padding = "16px 28px";
+        navbar.style.borderRadius = "18px";
+    }
+
+    });
